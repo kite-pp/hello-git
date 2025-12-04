8a967e93e7c6feed5439cc47b2da42ca59ab9297
Kibeom Lim <kiuten12345@naver.com>
2025-12-04 13:49:19 +0000

    C2: Second Commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
+      System.out.println("Changes after First Commit");
    }
+
 }
 
